8296fe3064ed5e6e1840ea56aedd1fe254d0fa44
mario siqueira <mario.siqueira@mulesoft.com>
2020-01-12 00:37:55 -0300

    updated styles

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('.navbar input[name=q]').focusin(function() {
-    $('.navbar').addClass('search-focused');
-  });
-
-  $('.navbar input[name=q]').focusout(function() {
-    $('.navbar').removeClass('search-focused');
+  $('#menuToggle').click(function(e) {
+    $('nav').toggleClass('closed');
   });
 });
