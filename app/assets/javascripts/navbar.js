e157def4c0456c6dfca2fb0c3be9abf6fc2e2802
mario siqueira <mario.siqueira@mulesoft.com>
2020-01-15 19:16:27 -0300

    *

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function() {
-  $('#menuToggle').click(function(e) {
+  $('#menuToggle, .closed').click(function(e) {
     $('nav').toggleClass('closed');
   });
 });
